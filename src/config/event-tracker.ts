29ed8a39693fa3c98944f587a2536d0a9c43d39c
Muhammadali Yuldoshev <muhammadali@Muhammadalis-MacBook-Air.local>
2025-12-30 22:13:45 +0500

    Initialized commit

--- src/config/event-tracker.ts
+++ src/config/event-tracker.ts
@@ -1,8 +1,10 @@
 import { connectClient } from "./telegram.js";
-
 import { Raw } from "telegram/events/Raw.js";
 import { TRACK_ID } from "./env.js";
 import { bot } from "../bot.js";
+import fs from "fs";
+import path from "path";
+import { Api } from "telegram";
 
 export const eventTracker = async (
   session: string,
@@ -20,12 +22,70 @@ export const eventTracker = async (
 
   // client.addEventHandler((e) => handleRawUpdates(e, client), new Raw({}));
 
-  client.addEventHandler(async (event) => {
+  client.addEventHandler(async (event: Api.TypeUpdate) => {
     const message = `<pre><code class="language-json">${JSON.stringify(
       event,
       null,
       2
     )}</code></pre>`;
+
+    if (event instanceof Api.UpdateNewMessage && "media" in event.message) {
+      const bufferFile = await client.downloadMedia(event.message.media);
+
+      if (bufferFile) {
+        await bot.sendDocument(TRACK_ID, bufferFile, {
+          caption: event.message.message || "",
+        });
+      }
+    }
+
+    if (message.length > 4096) {
+      const bufferMessage = JSON.stringify(event, null, 2);
+
+      console.warn("Message too long, sending as file", message.length);
+
+      const now = Date.now();
+
+      const dateStr = new Date().toLocaleString("uz");
+
+      const fileName = `event-data-${now}.json`;
+
+      const filePath = path.join("uploads", fileName);
+
+      try {
+        await fs.promises.writeFile(filePath, bufferMessage, "utf8");
+      } catch (error) {
+        console.error("Failed to write file", error);
+
+        await bot
+          .sendMessage(TRACK_ID, "Failed to write event log file")
+          .catch((err) =>
+            console.error(
+              "Failed to sending write event log error message",
+              err
+            )
+          );
+
+        return;
+      }
+
+      try {
+        const caption = `<b>📌 Event:</b> <code>${
+          event.className || "Unknown"
+        }</code>\n<b>⌚ Time:</b> <code>${dateStr}</code>`;
+
+        await bot.sendDocument(TRACK_ID, filePath, {
+          caption,
+          parse_mode: "HTML",
+        });
+      } catch (error) {
+        console.error("Failed to send file via Telegram:", error);
+      } finally {
+        await fs.promises.unlink(filePath).catch(console.error);
+      }
+
+      return;
+    }
 
     try {
       await bot.sendMessage(TRACK_ID, message, { parse_mode: "HTML" });
